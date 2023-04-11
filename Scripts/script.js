3d548f241102ced7dcfcaa7a257708e1a97dd5e0
fabienv4 <fabienv4@hotmail.fr>
2023-04-11 18:21:31 +0200

    Ajout Contact et intro

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,23 @@
 const entryWB = document.querySelector(".welcomeBox")
+const entryConct = document.querySelector(".contactBox")
 const entryH = document.querySelector("header")
 const entryF = document.querySelector("footer")
+const entryM = document.querySelector(".mainBox")
 const selectorEntry = document.querySelector(".lienEntree")
+const selectorContact = document.querySelector(".contact")
 
 selectorEntry.addEventListener("click", function () {
     entryWB.style.display = "none"
     entryH.style.display = "flex"
     entryF.style.display = "flex"
+    entryM.style.display = "flex"
+    entryConct.style.display = "none"
+})
+
+selectorContact.addEventListener("click", function () {
+    entryWB.style.display = "none"
+    entryH.style.display = "flex"
+    entryF.style.display = "flex"
+    entryM.style.display = "none"
+    entryConct.style.display = "flex"
 })
